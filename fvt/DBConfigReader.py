441c756b99596f32ddd1b9639556e9fbf81eb53b
David Patzer <davidpatzer@hotmail.com>
2019-06-24 20:07:13 +0200

    new class DBConfigReader.py

---
+++ fvt/DBConfigReader.py
@@ -0,0 +1,21 @@
+import configparser
+
+
+class DBConfigReader:
+
+    def __init__(self, config_filename):
+        self.configParser = configparser.ConfigParser()
+        self.configParser.read("configs\\" + config_filename)
+
+    def getHost(self):
+        pass
+        # return self.configParser["HOST"]
+
+    def getUses(self):
+        pass
+
+    def getPassword(self):
+        pass
+
+    def getDB(self):
+        pass
--- fvt/tests/test_database.py
+++ fvt/tests/test_database.py
@@ -12,8 +12,3 @@ class TestDatabase:
 
     def test_defaultDatabaseConfig(self):
         pass
-
-
-
-
-
