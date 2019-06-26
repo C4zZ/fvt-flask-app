c7244880c0f3758cc8972c4df65a2330a6611eae
David Patzer <davidpatzer@hotmail.com>
2019-06-26 20:40:18 +0200

    implemented DBConfigReader methods successfully

--- fvt/DBConfigReader.py
+++ fvt/DBConfigReader.py
@@ -1,22 +1,29 @@
 import configparser
+import os
 
 
 class DBConfigReader:
     """ reads host, user, password and db values from specified config file on database instantiation.
     """
     def __init__(self, config_filename):
-        with open("configs\\" + config_filename) as f:
-        self.configParser = configparser.ConfigParser()
-        self.configParser.read_file(f)
+
+        # getting path where this module (DBConfigReader.py) is located adn appending
+        # configs dir
+        filedir = os.path.dirname(os.path.realpath(__file__)) + "\\configs\\"
+
+
+        with open(filedir + config_filename, "r") as f:
+            self.configParser = configparser.ConfigParser()
+            self.configParser.read_file(f)
 
     def getHost(self):
-        return self.configParser["HOST"]
+        return self.configParser["Main"]["HOST"]
 
     def getUser(self):
-        return self.configParser["USER"]
+        return self.configParser["Main"]["USER"]
 
     def getPassword(self):
-        return self.configParser["PASSWORD"]
+        return self.configParser["Main"]["PASSWORD"]
 
     def getDB(self):
-        return self.configParser["DB"]
+        return self.configParser["Main"]["DB"]
--- fvt/tests/test_DBConfigReader.py
+++ fvt/tests/test_DBConfigReader.py
@@ -10,14 +10,14 @@ def TestConfigReader():
 
 class TestDBConfigReader:
 
-    def test_DBConfigReader_read_test_host(self):
-        pass
+    def test_DBConfigReader_read_test_host(self, TestConfigReader):
+        assert TestConfigReader.getHost() == "host"
 
-    def test_DBConfigReader_read_test_user(self):
-        pass
+    def test_DBConfigReader_read_test_user(self, TestConfigReader):
+        assert TestConfigReader.getUser() == "user"
 
-    def test_DBConfigReader_read_test_password(self):
-        pass
+    def test_DBConfigReader_read_test_password(self, TestConfigReader):
+        assert TestConfigReader.getPassword() == "secretpassword"
 
-    def test_DBConfigReader_read_test_db(self):
-        pass
+    def test_DBConfigReader_read_test_db(self, TestConfigReader):
+        assert TestConfigReader.getDB() == "database"
