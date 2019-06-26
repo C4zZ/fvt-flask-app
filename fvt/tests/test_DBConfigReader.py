e5b47ebda2b59d9727488695f49361c4629930f0
David Patzer <davidpatzer@hotmail.com>
2019-06-26 19:41:53 +0200

    test_DBConfigReader.py

---
+++ fvt/tests/test_DBConfigReader.py
@@ -0,0 +1,15 @@
+import pytest
+
+class TestDBConfigReader:
+
+    def test_DBConfigReader_read_test_host(self):
+        pass
+
+    def test_DBConfigReader_read_test_user(self):
+        pass
+
+    def test_DBConfigReader_read_test_password(self):
+        pass
+
+    def test_DBConfigReader_read_test_db(self):
+        pass
