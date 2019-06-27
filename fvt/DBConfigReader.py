b95b3c6648142042b2b0b30aadc3b6c3d4ae8bc6
David Patzer <davidpatzer@hotmail.com>
2019-06-27 19:51:51 +0200

    improved DBConfigReader comment

--- fvt/DBConfigReader.py
+++ fvt/DBConfigReader.py
@@ -7,14 +7,15 @@ class DBConfigReader:
     """
     def __init__(self, config_filename):
 
-        # getting path where this module (DBConfigReader.py) is located adn appending
+        # getting path where this module (DBConfigReader.py) is located and appending
         # configs dir
         filedir = os.path.dirname(os.path.realpath(__file__)) + "\\configs\\"
-
 
         with open(filedir + config_filename, "r") as f:
             self.configParser = configparser.ConfigParser()
             self.configParser.read_file(f)
+
+            # TODO: checking if the config file has a 'Main' section here
 
     def getHost(self):
         return self.configParser["Main"]["HOST"]
