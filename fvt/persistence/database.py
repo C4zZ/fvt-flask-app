d39e7049744f87c1e8714008fc426d4ccfe6ab9b
David Patzer <davidpatzer@hotmail.com>
2019-07-14 12:02:18 +0200

    doc comments for all other database methods

--- fvt/persistence/database.py
+++ fvt/persistence/database.py
@@ -8,8 +8,9 @@ database = conn.cursor(PyMySQLdb.cursors.Cursor)
 
 class PyMySQLDBConnection(object):
     """
-    database class for fvt-flask-app with custom methods for accessing needed verbs and track the input of the user
+    database class for fvt-flask-app with custom methods for accessing needed verbs and track the input of the user.
     """
+
     def __init__(self, config_filename="defaultDB.cfg"):
 
         self.connection = None
@@ -23,31 +24,43 @@ class PyMySQLDBConnection(object):
 
     def __enter__(self):
         """
-        method for creating connection for current database instance before 'with' statement
+        method for creating connection for current database instance before 'with' statement.
         """
+
         self.connection = PyMySQLdb.connect(host=self.host, user=self.user, password=self.password, db=self.db)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         """
-        method for closing connection for current database instance after 'with' statement
+        method for closing connection for current database instance after 'with' statement.
         """
         self.connection.close()
 
     def getColumnNamesFromTable(self, table_name):
+        """
+        getColumnNames requests all column names via below query,formats all column names as a list and returns this list.
+        :param table_name: the name of the table form which all column names should be fetched.
+        :return: list of all column names form table with table_name.
+        """
 
         with self as db:
             cursor = db.connection.cursor()
+
+            # be wary of sql injection here?
             query = "desc %s" % table_name
+
             cursor.execute(query)
 
             field_names = [column[0] for column in cursor.fetchall()]
 
-            d = 9
-
             return field_names
 
     def getTableNames(self):
+        """
+        getTableNames request all available table names form current database, puts all table names into a list and
+        returns it.
+        """
+
         with self as db:
             cursor = db.connection.cursor()
 
@@ -60,6 +73,19 @@ class PyMySQLDBConnection(object):
             return table_names_list
 
     def trackUserPerformance(self, verbform, verbsolution, erroneousUserInput, isVerbCorrect, date):
+        """
+        trackUserPerformance tracks whether the user typed in the correct verb or not. It documents the users input when
+        it was wrong and when this input was typed in.
+        :param verbform: the string containing person, number, tense and baseVerb which are important for the user to type
+        in the correct answer e.g. the grammatical form of the actual verb which the user needs to type in.
+        :param verbsolution: the correct verb build from the verbform. Needs to be documented for if the user types in
+        a wrong verb.
+        :param erroneousUserInput: the wrong input of the user if any
+        :param isVerbCorrect: 0 for the information that the user input was correct and 1 for the information that the
+        user input was incorrect.
+        :param date: the date when the user input happened in the format dd-mm-yyyy.
+        """
+
         with self as db:
             cursor = db.connection.cursor()
 
