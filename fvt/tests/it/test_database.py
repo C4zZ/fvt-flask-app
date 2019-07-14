b2d0c2d852459844248e0432b5087a750c2b9c5b
David Patzer <davidpatzer@hotmail.com>
2019-07-14 18:07:05 +0200

    first integration test for trackUserPerformance()

--- fvt/tests/it/test_database.py
+++ fvt/tests/it/test_database.py
@@ -12,12 +12,11 @@ class TestDatabase:
         setupDB.trackUserPerformance(verbform, verbsolution, erroneousUserInput, isVerbCorrect, date)
 
         cursor = setupDB.connection.cursor()
-        cursor.execute("SELECT * FROM `trackusersuccessfailure`;")
+        cursor.execute("SELECT * FROM trackusersuccessfailure")
 
-        res = cursor.fetchall()
-
-        cursor.close()
-        df = 34
-
-
-        # now test data needs to be rollbacked somehow?
+        res = cursor.fetchall()[0]
+        assert verbform in res and \
+                verbsolution in res and \
+                erroneousUserInput in res and \
+                int(isVerbCorrect) in res and \
+                date in res
