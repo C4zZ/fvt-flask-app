40174384b20b8a6384dc3ac69a19d16797e3bdd2
David Patzer <davidpatzer@hotmail.com>
2019-07-17 22:10:19 +0200

    added tests for productionApp_config_client fixture

--- fvt/tests/ut/test_config.py
+++ fvt/tests/ut/test_config.py
@@ -1,4 +1,3 @@
-
 class TestConfig:
     """
     tests for loading Flask.config form an .cfg file
@@ -9,3 +8,9 @@ class TestConfig:
     
     def test_testConfigTestingIsEnabled(self, testingApp_config_client):
         assert testingApp_config_client.testing == True
+
+    def test_productionConfigDebugIsDisabled(self, productionApp_config_client):
+        assert productionApp_config_client.debug == False
+
+    def test_productionConfigTestingIsDisabled(self, productionApp_config_client):
+        assert productionApp_config_client.testing == False
