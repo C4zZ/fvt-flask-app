17efa5bcac1d2d3f5891c8d86ac2448b15dab461
David Patzer <davidpatzer@hotmail.com>
2019-07-18 20:39:39 +0200

    fixture test_client is now correctly configured for testing via app.testing = True

--- fvt/tests/conftest.py
+++ fvt/tests/conftest.py
@@ -13,11 +13,24 @@ def fixture_default_client():
 def test_client():
     app = create_app()
 
-    # saving test_client() inside app so that unit tests are possible 
-    # e.g. that request methods like get() and post() are available
-    app = app.test_client()
+    # Taken from the doc comment of Flask().test_client() method:
+    # Note that if you are testing for assertions or exceptions in your
+    # application code, you must set ``app.testing = True`` in order for the
+    # exceptions to propagate to the test client.  Otherwise, the exception
+    # will be handled by the application (not visible to the test client) and
+    # the only indication of an AssertionError or other exception will be a
+    # 500 status code response to the test client.  See the :attr:`testing`
+    # attribute.  For example::
 
-    return app
+    #    app.testing = True
+    #    client = app.test_client()
+    app.testing = True
+
+    # saving app.test_client() inside app so that unit tests are possible
+    # e.g. that request methods like get() and post() are available
+    test_client = app.test_client()
+
+    return test_client
 
 
 @pytest.fixture
