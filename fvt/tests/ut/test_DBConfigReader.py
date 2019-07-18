9463717167bf2e8561ced6a92058049a9f37269b
David Patzer <davidpatzer@hotmail.com>
2019-07-18 21:10:41 +0200

    renamed fixture method TestConfigReader() to DB_TestConfigReader()

--- fvt/tests/ut/test_DBConfigReader.py
+++ fvt/tests/ut/test_DBConfigReader.py
@@ -4,24 +4,24 @@ from fvt.DBConfigReader import DBConfigReader
 
 
 @pytest.fixture()
-def TestConfigReader():
+def DB_TestConfigReader():
     configReader = DBConfigReader("DBTestConfig.cfg")
     return configReader
 
 
 class TestDBConfigReader:
 
-    def test_DBConfigReader_read_test_host(self, TestConfigReader):
-        assert TestConfigReader.getHost() == "host"
+    def test_DBConfigReader_read_test_host(self, DB_TestConfigReader):
+        assert DB_TestConfigReader.getHost() == "host"
 
-    def test_DBConfigReader_read_test_user(self, TestConfigReader):
-        assert TestConfigReader.getUser() == "user"
+    def test_DBConfigReader_read_test_user(self, DB_TestConfigReader):
+        assert DB_TestConfigReader.getUser() == "user"
 
-    def test_DBConfigReader_read_test_password(self, TestConfigReader):
-        assert TestConfigReader.getPassword() == "secretpassword"
+    def test_DBConfigReader_read_test_password(self, DB_TestConfigReader):
+        assert DB_TestConfigReader.getPassword() == "secretpassword"
 
-    def test_DBConfigReader_read_test_db(self, TestConfigReader):
-        assert TestConfigReader.getDB() == "database"
+    def test_DBConfigReader_read_test_db(self, DB_TestConfigReader):
+        assert DB_TestConfigReader.getDB() == "database"
 
-    def test_DBConfigReader_read_test_testing(self, TestConfigReader):
-        assert TestConfigReader.isTestingDB() == True
+    def test_DBConfigReader_read_test_testing(self, DB_TestConfigReader):
+        assert DB_TestConfigReader.isTestingDB() == True
