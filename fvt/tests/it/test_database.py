3d63ea73f402af98a1f7dcf4d4f7c4743e3a68f6
David Patzer <davidpatzer@hotmail.com>
2019-08-18 16:15:23 +0200

    tested all possibilities for database method buildprésent(...) for irregular verb être

--- fvt/tests/it/test_database.py
+++ fvt/tests/it/test_database.py
@@ -20,9 +20,23 @@ class TestDatabase:
         assert 1 in query
         assert 0 not in query[1:]
 
-    def test_buildpresent1(self, testDB):
+    def test_buildpresent_etre1(self, testDB):
         assert testDB.build_présent("être", "1", "Sg") == "suis"
 
+    def test_buildpresent_etre2(self, testDB):
+        assert testDB.build_présent("être", "2", "Sg") == "es"
+
+    def test_buildpresent_etre3(self, testDB):
+        assert testDB.build_présent("être", "3", "Sg") == "est"
+
+    def test_buildpresent_etre4(self, testDB):
+        assert testDB.build_présent("être", "1", "Pl") == "sommes"
+
+    def test_buildpresent_etre5(self, testDB):
+        assert testDB.build_présent("être", "2", "Pl") == "êtes"
+
+    def test_buildpresent_etre6(self, testDB):
+        assert testDB.build_présent("être", "3", "Pl") == "sont"
 
 def get_trackUserPerformance_results_for(db, verbform, verbsolution, erroneousUserInput, date):
     db.trackUserPerformance(verbform, verbsolution, erroneousUserInput, date)
